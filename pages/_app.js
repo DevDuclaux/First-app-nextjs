0fe44907142fe49fb44f445aaed8b596e6ac57ba
Duclaux Fredy <tagneduclaux@gmail.com>
2023-03-15 05:49:00 +0100

    ajout dur premier essai

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Layout from './layout/Head.js'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout></Layout>
+      <Component {...pageProps} />
+    </>
+    )
 }
